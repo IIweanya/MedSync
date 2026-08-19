c00460d9406c23874c3d33cb5b7442a94823be6d
IIweanya <ikechukwuiweanya09@gmail.com>
2026-08-19 08:44:12 +0100

    Configure Django for PostgreSQL

--- medsync/settings.py
+++ medsync/settings.py
@@ -1,5 +1,7 @@
 import os
 from pathlib import Path
+
+import dj_database_url
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -70,10 +72,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'medsync.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'
+    )
 }
 
 AUTH_USER_MODEL = 'accounts.User'
