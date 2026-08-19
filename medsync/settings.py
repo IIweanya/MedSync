a9ac96c1cd5be4c1dc17df6c310b5aa710387dd4
IIweanya <ikechukwuiweanya09@gmail.com>
2026-08-19 09:16:50 +0100

    Staged all files

--- medsync/settings.py
+++ medsync/settings.py
@@ -73,7 +73,9 @@ WSGI_APPLICATION = 'medsync.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600,
+        conn_health_checks=True,
     )
 }
 
